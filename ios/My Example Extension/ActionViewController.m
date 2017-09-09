e326b8ff9d29347fde598a6402a3f4065e35501f
Shane O'Sullivan <shaneosullivan1@gmail.com>
2017-09-08 23:42:45 -0700

    Make React Native UI show up

--- ios/My Example Extension/ActionViewController.m
+++ ios/My Example Extension/ActionViewController.m
@@ -8,6 +8,8 @@
 
 #import "ActionViewController.h"
 #import <MobileCoreServices/MobileCoreServices.h>
+#import <React/RCTBundleURLProvider.h>
+#import <React/RCTRootView.h>
 
 @interface ActionViewController ()
 
@@ -17,38 +19,51 @@
 
 @implementation ActionViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    
-    // Get the item[s] we're handling from the extension context.
-    
-    // For example, look for an image and place it into an image view.
-    // Replace this with something appropriate for the type[s] your extension supports.
-    BOOL imageFound = NO;
-    for (NSExtensionItem *item in self.extensionContext.inputItems) {
-        for (NSItemProvider *itemProvider in item.attachments) {
-            if ([itemProvider hasItemConformingToTypeIdentifier:(NSString *)kUTTypeImage]) {
-                // This is an image. We'll load it, then place it in our image view.
-                __weak UIImageView *imageView = self.imageView;
-                [itemProvider loadItemForTypeIdentifier:(NSString *)kUTTypeImage options:nil completionHandler:^(UIImage *image, NSError *error) {
-                    if(image) {
-                        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
-                            [imageView setImage:image];
-                        }];
-                    }
-                }];
-                
-                imageFound = YES;
-                break;
-            }
-        }
-        
-        if (imageFound) {
-            // We only handle one image, so stop looking for more.
-            break;
-        }
-    }
+- (void)loadView {
+  NSURL *jsCodeLocation;
+  
+  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
+  
+  RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
+                                                      moduleName:@"ReactNativeExampleBrowserExtension"
+                                               initialProperties:nil
+                                                   launchOptions:nil];
+  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  self.view = rootView;
 }
+
+//- (void)viewDidLoad {
+//    [super viewDidLoad];
+//    
+//    // Get the item[s] we're handling from the extension context.
+//    
+//    // For example, look for an image and place it into an image view.
+//    // Replace this with something appropriate for the type[s] your extension supports.
+//    BOOL imageFound = NO;
+//    for (NSExtensionItem *item in self.extensionContext.inputItems) {
+//        for (NSItemProvider *itemProvider in item.attachments) {
+//            if ([itemProvider hasItemConformingToTypeIdentifier:(NSString *)kUTTypeImage]) {
+//                // This is an image. We'll load it, then place it in our image view.
+//                __weak UIImageView *imageView = self.imageView;
+//                [itemProvider loadItemForTypeIdentifier:(NSString *)kUTTypeImage options:nil completionHandler:^(UIImage *image, NSError *error) {
+//                    if(image) {
+//                        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+//                            [imageView setImage:image];
+//                        }];
+//                    }
+//                }];
+//                
+//                imageFound = YES;
+//                break;
+//            }
+//        }
+//        
+//        if (imageFound) {
+//            // We only handle one image, so stop looking for more.
+//            break;
+//        }
+//    }
+//}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
